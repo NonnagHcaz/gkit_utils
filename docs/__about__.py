a5c67f14f0b6da46d665163fcc00e2fa04155e36
Zachary Gannon <zachgannon93@gmail.com>
2017-07-18 10:34:45 -0400

    Appveyor changes.

--- docs/__about__.py
+++ docs/__about__.py
@@ -15,6 +15,8 @@ __classifiers__ = '\n'.join([
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Topic :: Software Development :: Libraries :: Python Modules'
